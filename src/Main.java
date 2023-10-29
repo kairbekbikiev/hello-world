8943b98d9ca7b82374bb7a2fd7ecf10b6be919e5
Kairbekbikiev <kairbekbikiev@gmail.com>
2023-10-29 13:04:23 +0600

    add lesson 3.1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("\t\t\tДобро пожаловать в конвертер температур");
+                System.out.println("B " + 32 + " C :" + (32.0 * 1.8 + 32) + " F ");
+        }
+}
